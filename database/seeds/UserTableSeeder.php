d509c6fe5c2494b3bcc6a2defc6c25c4877ce3d9
ccastro12 <desarrollo1@afqsas.com>
2018-11-22 11:39:44 -0500

    Commit del Login

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/* Elementos necesarios para la ejecucion */
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $user = new User();
+        $user->name = 'Administrador';
+        $user->email = 'admin@tw.com';
+        $user->password = bcrypt('secret');
+        $user->estado = 1;
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Secretaria';
+        $user->email = 'secre@tw.com';
+        $user->password = bcrypt('secret');
+        $user->estado = 1;
+        $user->save();
+    }
+}
